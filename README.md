fc09b9d5ba36c3ed5f003fbeda071f6cb97f3e1c
Bogdan Crisan <bogdanrc.22@gmail.com>
2020-12-08 13:30:36 +0200

    added the final version of rhe Etch-A-Sketch Project

---
+++ README.md
@@ -0,0 +1,45 @@
+TITLE: 
+
+ Etch-A-Sketch
+
+VERSION NUMBER: 
+
+ 1.0
+
+DESCRIPTION: 
+
+ A drawing toy operated by the move of the mouse in the white area
+
+PROJECT STATUS: 
+
+ COMPLETE
+
+TO DO: 
+
+ -
+
+REQUIREMENTS: 
+
+ none
+
+USAGE GUIDE: 
+
+ - open the index.html file ad move the cursor around in the white area. 
+ - to increase the 'pixel' density the slider can be used
+ - to clear all the lines that have been added the Reset button is used
+
+TECHNOLOGIES USED: 
+
+ HTML, CSS, DOM
+
+WHAT I'VE LEARNED: 
+
+ - loop through DOM elements
+ - what node lists are and how they work
+ - use event listeners
+ - use event listeners on HTML input
+ - add new elements to the DOM if certain conditions are met
+ - apply styles with object methods
+
+MOTIVATION: 
+ - to better understand the DOM and how create elements using JS instead of HTML
---
+++ script.js
@@ -0,0 +1,94 @@
+const input = document.querySelector('input');
+const rangeText = document.getElementById('rangetext')
+const sketchArea = document.querySelector(`#sketcharea`);
+const resetButton = document.getElementById('btn__reset');
+const blackWhiteButton = document.getElementById('btn__bw');
+
+
+function createElem() {
+    let newElem = document.createElement('div');
+    newElem.classList.add('griditem');
+    sketchArea.appendChild(newElem);
+}
+
+function deleteElem() {
+    for (let i = sketchArea.childNodes.length; i > 0; i--) {
+        sketchArea.innerHTML = '';
+    }
+}
+
+function updateGrid() {
+    if (input.value == 1) {
+        deleteElem();
+        sketchArea.setAttribute('style', 'grid-template-columns: repeat(4, 1fr);');
+        rangeText.textContent = "4";
+        for (let i = 0; i < 16; i++) {
+            createElem();
+        }
+        addElemBlackStyle();
+    } else if (input.value == 2) {
+        deleteElem();
+        sketchArea.setAttribute('style', 'grid-template-columns: repeat(8, 1fr);');
+        rangeText.textContent = "8";
+        for (let i = 0; i < 64; i++) {
+            createElem();
+        }
+        addElemBlackStyle();
+    } else if (input.value == 3) {
+        deleteElem();
+        sketchArea.setAttribute('style', 'grid-template-columns: repeat(16, 1fr);');
+        rangeText.textContent = "16";
+        for (let i = 0; i < 256; i++) {
+            createElem();
+        }
+        addElemBlackStyle()
+    } else if (input.value == 4) {
+        deleteElem();
+        sketchArea.setAttribute('style', 'grid-template-columns: repeat(32, 1fr);');
+        rangeText.textContent = "32";
+        for (let i = 0; i < 1024; i++) {
+            createElem();
+        }
+        addElemBlackStyle();
+    }
+}
+let colorValue = '';
+function colorRandomizer() {
+    let randomNumber = Math.random();
+
+    if (randomNumber < 0.25) {
+        colorValue = '#e8e8e8';
+    } else if (randomNumber < 0.5) {
+        colorValue = '#f05454';
+    } else if (randomNumber < 0.75) {
+        colorValue = '#30475e';
+    } else {
+        colorValue = '#222831';
+    }
+}
+
+function addElemBlackStyle() {
+    const gridItem = document.querySelectorAll('.griditem');
+
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener('mouseover', () => {
+            colorRandomizer()
+            gridItem[i].style.backgroundColor = colorValue;
+        });
+    }
+}
+
+updateGrid(); // called to update the function upon initial page load
+
+input.addEventListener('change', updateGrid)
+
+
+resetButton.addEventListener('click', () => {
+    const gridItem = document.querySelectorAll('.griditem');
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].style.backgroundColor = `white`;
+    }
+});
+
+
+
